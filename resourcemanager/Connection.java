b3a276826cd96a5eef5090e2883565b5960a774c
Barla Sai <Barla.Sai@godigit.com>
2024-08-05 15:43:26 +0530

    [MEGHANA]ADD: Program for Exception handling with Resource CleanUp

---
+++ resourcemanager/Connection.java
@@ -0,0 +1,18 @@
+
+package ResourceManager;
+
+class Connection implements AutoCloseable {
+    public void open() {
+        System.out.println("Connection opened.");
+    }
+    public void execute(String query) throws Exception {
+        if (query == null) {
+            throw new Exception("Query cannot be null");
+        }
+        System.out.println("Executing query: " + query);
+    }
+    @Override
+    public void close() {
+        System.out.println("Connection closed.");
+    }
+}
