e77c82e6c9a7f6f9c413441074a434b390f747ea
Barla Sai <Barla.Sai@godigit.com>
2024-08-07 15:54:53 +0530

    [MEGHANA]ADD: Copy file

---
+++ copyfile/Main.java
@@ -0,0 +1,18 @@
+
+package copyfile;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Path sourcePath = Paths.get("src/copyfile/source");
+        Path destPath = Paths.get("src/copyfile/destination");
+        try {
+            Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File copied successfully");
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+}
